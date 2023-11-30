fffe6dbb8d45c0b01fe86ac2b35f9782468f5f26
Priyank Trivedi <priyank@inheritx.com>
2023-11-30 17:48:05 +0530

    commented p tag in app.js

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
         </p> */}
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://eatsy.be/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Eatsy
         </a>
       </header>
     </div>
